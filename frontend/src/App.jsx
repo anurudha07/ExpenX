9a417aebffe2235d0636a957b576ff22ea2abca0
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-07-01 09:39:28 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ import Logout from "./pages/Auth/Logout";
 import Home from "./pages/Dashboard/Home";
 import Income from "./pages/Dashboard/Income";
 import Expense from "./pages/Dashboard/Expense";
-import UserProvider from './context/userContext';
+import UserProvider from './context/UserContext';
 import { Toaster } from 'react-hot-toast';
 import RequireAuth from './components/RequireAuth';
 
